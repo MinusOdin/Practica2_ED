e057871377d7532cff3a2dc1ac643ecf347184cd
arbeljul11@gmail.com <arbeljul21>
2022-03-12 12:03:32 +0100

    First Commit

---
+++ CuaEstatica.h
@@ -0,0 +1,10 @@
+#ifndef CUAESTATICA_H
+#define CUAESTATICA_H
+
+class CuaEstatica {
+    public:
+        CuaEstatica(const int max_size);
+        virtual ~CuaEstatica();
+        void ~CuaEstatica();
+        void enqueue(const int key)
+}
